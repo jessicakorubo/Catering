e9cd82b47eedd688478cef9db517fb4a0bf03245
Jessica <jessicakorubo@gmail.com>
2023-02-03 11:02:37 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // FUNCTION TO OPEN MENU ONCE YOU CLICK ON HAMBURGER
+
+let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.responsiveMenu');
+let close = document.querySelector('.close')
+
+hamburger.addEventListener('click', ()=>{
+    menu.classList.toggle('active');
+    console.log("Clicked on Menu Button!")
+});
+
+close.addEventListener('click', ()=>{
+    menu.classList.remove('active');
+})
